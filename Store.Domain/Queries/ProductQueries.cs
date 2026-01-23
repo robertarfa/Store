6d7b66c8e0b1bb2193c205f7eaa1f12f0e9366bb
Roberta Aguilar <roberta_rfa@hotmail.com>
2026-01-23 08:30:54 -0300

    Feat: Expression

---
+++ Store.Domain/Queries/ProductQueries.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+using Store.Domain.Entities;
+
+namespace Store.Domain.Queries
+{
+    public static class ProductQueries
+    {
+        public static Expression<Func<Product, bool>> GetActiveProducts()
+        {
+            return x => x.Active;
+        }
+
+        public static Expression<Func<Product, bool>> GetInactiveProducts()
+        {
+            return x => !x.Active;
+        }
+    }
+}
