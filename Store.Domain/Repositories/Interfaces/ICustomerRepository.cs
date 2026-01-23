27009a7ac436ce41d0d07c3baae1bb1035e0a43c
Roberta Aguilar <roberta_rfa@hotmail.com>
2026-01-23 08:31:13 -0300

    Feat: Repository Interfaces

---
+++ Store.Domain/Repositories/Interfaces/ICustomerRepository.cs
@@ -0,0 +1,8 @@
+using Store.Domain.Entities;
+namespace Store.Domain.Repositories.Interfaces
+{
+    public interface ICustomerRepository
+    {
+        Customer Get(string document);
+    }
+}
---
+++ Store.Domain/Repositories/Interfaces/IProductRepository.cs
@@ -0,0 +1,11 @@
+using System.Collections.Generic;
+using System;
+using Store.Domain.Entities;
+
+namespace Store.Domain.Repositories.Interfaces
+{
+    public interface IProductRepository
+    {
+        IEnumerable<Product> Get(IEnumerable<Guid> ids);
+    }
+}
