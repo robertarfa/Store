1445feb6a3074451c6e54375e3bc80de166517b2
Roberta Aguilar <roberta_rfa@hotmail.com>
2026-01-28 08:38:56 -0300

    Feat: Command

---
+++ Store.Domain/Commands/CreateOrderCommand.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Flunt.Notifications;
+using Flunt.Validations;
+using Store.Domain.Commands.Interfaces;
+
+namespace Store.Domain.Commands
+{
+    public class CreateOrderCommand : Notifiable, ICommand
+    {
+        public CreateOrderCommand()
+        {
+            Items = new List<CreateOrderItemCommand>();
+        }
+
+        public CreateOrderCommand(string customer, string zipCode, string promoCode)
+        {
+            Customer = customer;
+            ZipCode = zipCode;
+            PromoCode = promoCode;
+        }
+        public string Customer { get; set; }
+        public string ZipCode { get; set; }
+        public string PromoCode { get; set; }
+        public IList<CreateOrderItemCommand> Items { get; set; }
+        public void Validate()
+        {
+            AddNotifications(
+                new Contract()
+                .Requires()
+                .HasLen(Customer, 11, "Customer", "Cliente inválido")
+                .HasLen(ZipCode, 8, "ZipCode", "CEP inválido")
+            );
+        }
+    }
+}
---
+++ Store.Tests/Commands/CreateOrderCommandTests.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Store.Domain.Commands;
+
+namespace Store.Tests.Commands
+{
+    [TestClass]
+    public class CreateOrderCommandTests
+    {
+        [TestMethod]
+        [TestCategory("Handlers")]
+        public void Dado_um_comando_invalido_o_pedido_nao_deve_ser_gerado()
+        {
+            var command = new CreateOrderCommand();
+            command.Customer = "";
+            command.ZipCode = "09551010";
+            command.PromoCode = "123589639";
+            command.Items.Add(new CreateOrderItemCommand(Guid.NewGuid(), 1));
+            command.Items.Add(new CreateOrderItemCommand(Guid.NewGuid(), 1));
+            command.Validate();
+
+            Assert.AreEqual(command.Valid, false);
+        }
+    }
+}
